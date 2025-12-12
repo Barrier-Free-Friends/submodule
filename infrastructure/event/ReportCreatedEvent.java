a127beac20f52a18476c369fc11b0fc554120666
jinnk0 <jin8073@gmail.com>
2025-12-12 17:32:42 +0900

    add: 카프카 이벤트 공통 모듈에 추가

---
+++ infrastructure/event/ReportCreatedEvent.java
@@ -0,0 +1,25 @@
+package org.bf.global.infrastructure.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.bf.global.domain.event.AbstractDomainEvent;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReportCreatedEvent extends AbstractDomainEvent {
+    private UUID userId;
+    private int point;
+    private UUID sourceId;
+    private String sourceTable;
+
+    @Override
+    public String getTopicName() {
+        return "report-events";
+    }
+}
---
+++ infrastructure/event/ReportDeletedEvent.java
@@ -0,0 +1,24 @@
+package org.bf.global.infrastructure.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.bf.global.domain.event.AbstractDomainEvent;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReportDeletedEvent extends AbstractDomainEvent {
+    private UUID userId;
+    private UUID sourceId;
+    private String sourceTable;
+
+    @Override
+    public String getTopicName() {
+        return "report-events";
+    }
+}
