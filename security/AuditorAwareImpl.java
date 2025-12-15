2b29dacf8a5697c244d827ac2242df18081131f8
jinnk0 <jin8073@gmail.com>
2025-12-15 10:23:43 +0900

    fix: auditable 필드 자동 주입 설정 수정

--- security/AuditorAwareImpl.java
+++ security/AuditorAwareImpl.java
@@ -3,9 +3,11 @@ package org.bf.global.security;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
+@Component
 public class AuditorAwareImpl implements AuditorAware<String> {
 
     /**
@@ -35,12 +37,5 @@ public class AuditorAwareImpl implements AuditorAware<String> {
         }
 
         return Optional.empty();
-    }
-
-    /**
-     * 익명 사용자 여부를 확인하는 헬퍼 메서드
-     */
-    private boolean isAnonymous(Authentication authentication) {
-        return authentication.getPrincipal().equals("anonymousUser");
     }
 }
