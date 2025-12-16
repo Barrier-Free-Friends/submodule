407d7e7b85c86d618433c0ef613d10eeeb06e5f4
Yejin-source <yejin3831@naver.com>
2025-12-16 13:47:59 +0900

    fix: ReportMapImageInfo 주소값 삭제

--- infrastructure/event/ReportMapImageInfo.java
+++ infrastructure/event/ReportMapImageInfo.java
@@ -2,6 +2,5 @@ package org.bf.global.infrastructure.event;
 
 public record ReportMapImageInfo(
         Double latitude,
-        Double longitude,
-        String address
+        Double longitude
 ) {}
