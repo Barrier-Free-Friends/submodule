9b743087ce02b1f860600b1adbe2fffa13cbc52b
RCNR <iop04226@gmail.com>
2025-12-19 15:33:36 +0900

    fix: global 서브모듈 수정

--- infrastructure/config/SwaggerConfig.java
+++ infrastructure/config/SwaggerConfig.java
@@ -1,11 +1,7 @@
 package org.bf.global.infrastructure.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
@@ -24,24 +20,12 @@ public class SwaggerConfig {
     }
 
     @Bean
-    @ConditionalOnMissingBean(OpenAPI.class)
     public OpenAPI customOpenAPI(){
-
-        Info info = new io.swagger.v3.oas.models.info.Info()
-                .title("Spot/Collection Service API")
-                .description("API documentation for Spot and Collection services");
-
-        String securityScheme = "JWT TOKEN";
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList(securityScheme);
-        Components components = new Components()
-                .addSecuritySchemes(securityScheme, new SecurityScheme()
-                        .name(securityScheme)
-                        .type(SecurityScheme.Type.HTTP).scheme("Bearer").bearerFormat("JWT"));
-
         return new OpenAPI()
-                .info(info)
-                .addServersItem(new Server().url("/"))
-                .addSecurityItem(securityRequirement)
-                .components(components);
+                .info(new Info()
+                        .title("Spring Boot Rest API")
+                        .version("1.0")
+                        .description("API documentation for your Spring Boot application")
+                );
     }
 }
