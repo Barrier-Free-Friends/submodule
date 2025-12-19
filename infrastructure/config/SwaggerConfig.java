d661b44f6a62e23bcbe198a1bc96499ba8c0c8cd
RCNR <iop04226@gmail.com>
2025-12-19 15:08:14 +0900

    refactor: 유저 id가 필요한 로직 재구성(#16)

--- infrastructure/config/SwaggerConfig.java
+++ infrastructure/config/SwaggerConfig.java
@@ -1,7 +1,11 @@
 package org.bf.global.infrastructure.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,11 +23,22 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI(){
+
+        Info info = new io.swagger.v3.oas.models.info.Info()
+                .title("Spot/Collection Service API")
+                .description("API documentation for Spot and Collection services");
+
+        String securityScheme = "JWT TOKEN";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(securityScheme);
+        Components components = new Components()
+                .addSecuritySchemes(securityScheme, new SecurityScheme()
+                        .name(securityScheme)
+                        .type(SecurityScheme.Type.HTTP).scheme("Bearer").bearerFormat("JWT"));
+
         return new OpenAPI()
-                .info(new Info()
-                        .title("Spring Boot Rest API")
-                        .version("1.0")
-                        .description("API documentation for your Spring Boot application")
-                );
+                .info(info)
+                .addServersItem(new Server().url("/"))
+                .addSecurityItem(securityRequirement)
+                .components(components);
     }
 }
